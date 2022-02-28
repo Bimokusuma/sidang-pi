eb67ea0b3fe1eb36aa35051bf5f33203df6020ec
Bimokusuma <100523577+Bimokusuma@users.noreply.github.com>
2022-02-28 14:15:07 +0700

    Update asgi.py

--- kuis_pintar/asgi.py
+++ kuis_pintar/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kuis_pintar.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sidang-pi.settings')
 
 application = get_asgi_application()
